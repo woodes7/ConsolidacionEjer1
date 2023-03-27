7150bd4b9ffdc0fdbbe75fd125fd1b1d965d4584
woodes7 <115068112+woodes7@users.noreply.github.com>
2023-03-27 11:09:01 +0200

    ejercicio de Condicionales comprovacion de edad

---
+++ Condicionales/src/main/webapp/script/scriptCondicionales.js
@@ -0,0 +1,11 @@
+function edad(){
+	var siesmayor=Number(prompt("Introduce tu edad: "));
+	
+	if (edad >= 18) {
+       alert('Tiene permiso para acceder.');
+    } else {
+      alert('No se permite acceso a menores de edad.');
+    }
+}
+
+  
